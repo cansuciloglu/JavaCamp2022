ce6ed205bb4d1a364b096e5425e14a0c0680540e
Cansu <cansuciiloglu@gmail.com>
2022-10-05 23:29:11 +0300

    week3

---
+++ Day3/oopWithNLayeredApp/src/oopWithNLayeredApp/Main.java
@@ -0,0 +1,22 @@
+package oopWithNLayeredApp;
+
+import oopWithNLayeredApp.business.ProductManager;
+import oopWithNLayeredApp.core.logging.DatabaseLogger;
+import oopWithNLayeredApp.core.logging.Logger;
+import oopWithNLayeredApp.core.logging.MailLogger;
+import oopWithNLayeredApp.dataAccess.JdbcProductDao;
+import oopWithNLayeredApp.entities.Product;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		Product product1 = new Product(1,"Iphone Xr",10000);
+		
+		Logger[] loggers = {new DatabaseLogger() , new MailLogger()};
+		
+		ProductManager productManager = new ProductManager(new JdbcProductDao() , loggers);
+		productManager.add(product1);
+
+	}
+
+}
